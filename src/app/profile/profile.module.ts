c3c18cd4c7fdebde58dd7990861185c2d716c4a0
Patel Krushankant <patelkk1618@gmail.com>
2020-07-29 16:14:19 -0500

    Header and Footer component Added.

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,9 +1,13 @@
+//Module
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
-import { ProfileComponent } from './profile.component';
 import {NgxSpinnerModule} from "ngx-spinner";
+//Component
+import { ProfileComponent } from './profile.component'
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
   imports: [
@@ -13,7 +17,9 @@ import {NgxSpinnerModule} from "ngx-spinner";
     NgxSpinnerModule
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   providers: [
     { provide: 'SnotifyToastConfig', useValue: ToastDefaults},
