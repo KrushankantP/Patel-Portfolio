219f0cede67f8c380c3e481b2c823006d6dece27
Patel Krushankant <patelkk1618@gmail.com>
2020-08-01 13:11:03 -0500

    Created Modal for Experience.

---
+++ src/app/Modal/IExperience.ts
@@ -0,0 +1,10 @@
+export interface IExperience {
+  company_Name: string;
+  role: string;
+  duration_years: string;
+  point_1: string;
+  point_2: string;
+  point_3: string;
+  point_4: string;
+  point_5: string;
+}
